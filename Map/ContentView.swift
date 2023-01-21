9eea2329645fa956ec149cfbf1d494545c2e5b70
Hemanth Kumar <70285394+Hemanth-Kumar-G@users.noreply.github.com>
2023-01-21 23:07:03 +0530

    map

--- Map/ContentView.swift
+++ Map/ContentView.swift
@@ -6,16 +6,43 @@
 //
 
 import SwiftUI
+import MapKit
 
+
+struct Location :Identifiable{
+    
+    let id = UUID()
+    let name:String
+    let coordinate : CLLocationCoordinate2D
+    
+}
 struct ContentView: View {
+    @State private var mapRegion = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 51.5, longitude: -0.12), span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)){
+        didSet{
+            print(mapRegion.center.latitude)
+        }
+    }
+    
+    let locations =  [
+        Location(name: "Buckingham", coordinate: CLLocationCoordinate2D(latitude: 51.501, longitude: -0.141))
+    ]
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Map(coordinateRegion: $mapRegion , annotationItems :  locations ){ location in
+//                MapMarker(coordinate: location.coordinate)
+                MapAnnotation(coordinate: location.coordinate,  content: {
+                    VStack{
+                    Circle()
+                        .stroke(.red, lineWidth: 3.0)
+                        .frame(width: 50,height: 50)
+                        
+                        Text(location.name).foregroundColor(.red).font(.title).fontWeight(.bold)
+                    }
+                })
+            }
         }
-        .padding()
+      
     }
 }
 
