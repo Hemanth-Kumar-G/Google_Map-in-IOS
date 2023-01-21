657c5d48adb7f547ca9cc1cf63f539b66fc6ed92
Hemanth Kumar <70285394+Hemanth-Kumar-G@users.noreply.github.com>
2023-01-21 22:07:14 +0530

    Initial Commit

---
+++ Map/MapApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapApp.swift
+//  Map
+//
+//  Created by HEMANTH on 21/01/23.
+//
+
+import SwiftUI
+
+@main
+struct MapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
